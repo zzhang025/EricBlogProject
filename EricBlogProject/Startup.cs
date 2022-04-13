eadfc5d30e47c74bc15440e7cd2a7559470ca2cb
zzhang025 <zhangzx45@gmail.com>
2022-04-12 19:57:30 -0700

    Data Migration

--- EricBlogProject/Startup.cs
+++ EricBlogProject/Startup.cs
@@ -28,9 +28,11 @@ namespace EricBlogProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlite(
+            //        Configuration.GetConnectionString("DefaultConnection"))); //Postgre or MS SQl
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddIdentity<BlogUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
