4ff901ed7897da96a593486c97541dce76c997b6
zzhang025 <zhangzx45@gmail.com>
2022-04-18 15:12:43 -0700

    Identity_Scalffording

---
+++ EricBlogProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using EricBlogProject.Data;
+using EricBlogProject.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EricBlogProject.Areas.Identity.IdentityHostingStartup))]
+namespace EricBlogProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
